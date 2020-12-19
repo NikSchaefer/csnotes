286f0fc2268370bce20400bc54a73a95c40f1094
Nik Schaefer <nikkschaefer@gmail.com>
2020-12-19 13:46:23 -0600

    wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 from django.core.management import call_command
@@ -6,3 +7,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_wsgi_application()
 call_command("migrate")
+
+sysout = sys.stdout
+sys.stdout = open('db.json', 'r')
+call_command('loaddata', 'db.json')
+sys.stdout = sysout
