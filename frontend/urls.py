219f4da9aa5b0fd11a76958e5157fc67fb56c057
Nik Schaefer <nikkschaefer@gmail.com>
2020-12-24 18:24:39 -0600

    build

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path, re_path
 from django.shortcuts import render
 
-
 # Views and Urls for frontend are combined for simplicity.
 def index(request, *args, **kwargs):
     return render(request, 'frontend/index.html')
-
 
 # urls here
 urlpatterns = [
