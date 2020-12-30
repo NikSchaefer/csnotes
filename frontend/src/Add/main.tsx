832c44143f0696c7a96d90cddbff8460e76adfcd
Nik Schaefer <nikkschaefer@gmail.com>
2020-12-30 14:09:50 -0600

    Contribute Main Page

--- frontend/src/Add/main.tsx
+++ frontend/src/Add/main.tsx
@@ -7,6 +7,19 @@ export default function AddDataset() {
             <p className='text'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
+            <div className="contribute-div">
+                <h2>How you can contribute</h2>
+                <a href='/add/api'>List of APIs</a>
+
+                <a href='/add/devtools'>Developer Tools</a>
+
+                <a href='/add/tools'>Frameworks and Providers</a>
+
+                <a href='/add/student'>Student Resources</a>
+
+                <a href='/add/more'>More</a>
+
+            </div>
         </div>
     )
 }
